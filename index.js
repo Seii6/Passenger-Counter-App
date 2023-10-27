022cd3aa6861aa25a760184d73f6a8c542e39b0d
Seii <142188232+Seii6@users.noreply.github.com>
2023-10-27 11:55:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,10 @@
 // count = count + 1
 // console.log(count)
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
-
+// intialize the count as 0 
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
 
 
